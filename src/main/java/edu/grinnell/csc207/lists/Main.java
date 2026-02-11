1fd3aae52b732e22b3178e2f3681bbee324065ad
leria-kosse <49876281+leria-kosse@users.noreply.github.com>
2026-02-11 10:20:16 -0600

    Initial commit

---
+++ src/main/java/edu/grinnell/csc207/lists/Main.java
@@ -0,0 +1,16 @@
+package edu.grinnell.csc207.lists;
+
+/**
+ * The driver for our lab on lists.
+ */
+public class Main {
+    /**
+     * The main entry point for the program.
+     * 
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nothing to do here!");
+        System.out.println("Check your implementation via the test suite.");
+    }
+}
